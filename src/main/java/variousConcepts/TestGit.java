a7dfc956dc7b942e921899c43bb651f4db91e5fa
shihabamin <amin.khandaker@gmail.com>
2021-01-17 13:51:04 -0600

    2nd commit

---
+++ src/main/java/variousConcepts/TestGit.java
@@ -0,0 +1,13 @@
+package variousConcepts;
+
+public class TestGit {
+	
+	public static void main(String[] args) {
+		printMe();
+	}
+	
+	public static void printMe() {
+		System.out.println("Test Git");
+	}
+
+}
